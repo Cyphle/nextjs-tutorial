d98d988a81d2c282a8b8771c3cd93af9f393d6b2
Cyphle <cyril.phamle@gmail.com>
2023-11-12 20:00:06 +0100

    get static props

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3808fb6323dc8bac"; }
+/******/ 		__webpack_require__.h = function() { return "f5141086043ce3a9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
